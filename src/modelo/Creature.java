9a98e0f60ce8659586b203e6164d037a97d62bd1
raquelnavarrogit <159776474+raquelnavarrogit@users.noreply.github.com>
2024-03-23 17:27:47 +0100

    correción gets and sets on Creature.java

--- src/modelo/Creature.java
+++ src/modelo/Creature.java
@@ -8,6 +8,57 @@ public abstract class Creature {
   private Weapon weapon;
   private Armour armour;
 
+  public String getName() {
+		return nombre;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+  public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+  public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+  public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   public abstract void speak();
   public void attack(Creature enemy){
     // Los métodos getters y setters están por crear
